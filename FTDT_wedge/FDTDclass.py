727609176924d7c451c578715fb6751fa3f9bd28
bodybilis <157311161+bodybilis@users.noreply.github.com>
2024-12-24 23:52:40 +0200

    for now PML is done, wedge next
    Current PML implementation can be called with various parameters to tune reflections. Total absorption at outer layer, still some mismatch in inner side, mcuh more reduced.
    current implementation is exponential form. 
    If I have the energy and we want to perfect it later, I can set-up a more automatic testing of reflections so that we can do a parameter scan with the new variables I introduced in last commit.

--- FTDT_wedge/FDTDclass.py
+++ FTDT_wedge/FDTDclass.py
@@ -6,140 +6,124 @@ from matplotlib.animation import ArtistAnimation
 class FDTD():
 
     def __init__(self):
-             #all the physical constants we need
-            self.c = 340 #[m/s] speed of sound
-            #miscelaneous
-            self.t0 =1E-3 #time delay
-        
-           
-           
-            #initialize the fields
-            self.plane_wave_init()
-            self.discritization()
-            self.ox = np.zeros((self.nx+1, self.ny),dtype=np.float64)
-            self.oy = np.zeros((self.nx, self.ny+1),dtype=np.float64)                 # when memory problems arise, can try float32
-            self.p = np.zeros((self.nx, self.ny),dtype=np.float64)     
+        # all the physical constants we need
+        self.c = 340  # [m/s] speed of sound
+        # miscelaneous
+        self.t0 = 1E-3  # time delay
 
-    
+        # initialize the fields
+        self.plane_wave_init()
+        self.discritization()
+        self.ox = np.zeros((self.nx + 1, self.ny), dtype=np.float64)
+        self.oy = np.zeros((self.nx, self.ny + 1), dtype=np.float64)  # when memory problems arise, can try float32
+        self.p = np.zeros((self.nx, self.ny), dtype=np.float64)
 
+        # receivers and sources
 
-            #receivers and sources 
+        self.x_source = int(self.nx / 2)  # Source is at the center
+        self.y_source = int(self.ny / 2)
 
-            self.x_source = int(self.nx / 2)  # Source is at the center
-            self.y_source = int(self.ny / 2)
+        self.A = 1  # Source amplitude
+        self.x_rec1 = int(self.x_source + self.d / self.dx)
+        self.y_rec1 = self.y_source + int(1.5 * self.d / self.dx)
+        self.x_rec2 = int(self.x_source + 2 * self.d / self.dx)
+        self.y_rec2 = self.y_rec1
+        self.x_rec3 = int(self.x_source + 3 * self.d / self.dx)
+        self.y_rec3 = self.y_rec1
 
-
-            self.A = 1  # Source amplitude
-            self.x_rec1 = int(self.x_source + self.d / self.dx)
-            self.y_rec1 = self.y_source + int(1.5 * self.d / self.dx)
-            self.x_rec2 = int(self.x_source + 2 * self.d / self.dx)
-            self.y_rec2 = self.y_rec1
-            self.x_rec3 = int(self.x_source + 3 * self.d / self.dx)
-            self.y_rec3 = self.y_rec1
-
-            
-    def plane_wave_init(self,d=20):
+    def plane_wave_init(self, d=20):
 
         """
         do this before discritization
-        also this also before initializng the parameters of the wavesource 
+        also this also before initializng the parameters of the wavesource
         """
 
-        self.d=d
+        self.d = d
 
-        self.f_min=self.c * 0.1 /(np.pi*d)
-        self.f_max=self. c * 4 /(np.pi*d)
-        self.fc = (self.f_min + self.f_max ) / 2               # central frequency
+        self.f_min = self.c * 0.1 / (np.pi * d)
+        self.f_max = self.c * 4 / (np.pi * d)
+        self.fc = (self.f_min + self.f_max) / 2  # central frequency
         self.bandwidth = self.f_max - self.f_min
 
         self.l_min = np.pi * self.d / 2
         self.l_max = np.pi * self.d * 20
-        self.sigma = (1 / (2 * self.bandwidth)) **2            # width of pulse according to literature
+        self.sigma = (1 / (2 * self.bandwidth)) ** 2  # width of pulse according to literature
         return None
-    
 
     def discritization(self):
-        self.dx = 0.1 * self.l_min                          #spatial discretisation of lambda_min
-        self.dy = self.dx 
+        self.dx = 0.1 * self.l_min  # spatial discretisation of lambda_min
+        self.dy = self.dx
 
         # can still reduce it
 
-        self.nx = int( self.l_max / self.dx)
+        self.nx = int(self.l_max / self.dx)
         self.ny = self.nx
 
         self.CFL = 1
 
-        self.dt = self.CFL / (self.c * np.sqrt((1 / self.dx ** 2) + (1 / self.dy ** 2)))    # time step from spatial disc. & CFL
+        self.dt = self.CFL / (
+                    self.c * np.sqrt((1 / self.dx ** 2) + (1 / self.dy ** 2)))  # time step from spatial disc. & CFL
         self.nt = int(self.nx // self.CFL)
         return None
-    
 
     def step_SIT_SIP(self):
-        
 
-        #need to change these to update slicing upto 1 cell before the wedge (1)
-        self.ox[1:-1, :] += -self.dt / self.dx * ( self.p[1:, :] - self.p[:-1, :] ) * np.exp( -self.dampx[1:-1,None])
-        self.oy[:, 1:-1] += -self.dt / self.dy * ( self.p[:, 1:] - self.p[:, :-1] ) * np.exp( -self.dampx[None, 1:-1,])
-        self.p += -self.c**2*self.dt*(1/self.dx*(self.ox[1:, :] - self.ox[:-1, :])  * np.exp( -self.dampx[:-1,None]) +
-                  + 1/self.dy*(self.oy[:, 1:] - self.oy[:, :-1]) *  np.exp( -self.dampy[None,1:])
-                  )
-        self.ox = self.ox * np.exp( -self.dampx[:,None])
-        self.oy = self.oy * np.exp( -self.dampy[None:,])
-        self.p = self.p * np.exp(- self.dampx[:-1,None ] )
+        # need to change these to update slicing upto 1 cell before the wedge (1)
+        self.ox[1:-1, :] += -self.dt / self.dx * (self.p[1:, :] - self.p[:-1, :]) * np.exp(-self.dampx[1:-1, None])
+        self.oy[:, 1:-1] += -self.dt / self.dy * (self.p[:, 1:] - self.p[:, :-1]) * np.exp(-self.dampx[None, 1:-1, ])
+        self.p += -self.c ** 2 * self.dt * (
+                    1 / self.dx * (self.ox[1:, :] - self.ox[:-1, :]) * np.exp(-self.dampx[:-1, None]) +
+                    + 1 / self.dy * (self.oy[:, 1:] - self.oy[:, :-1]) * np.exp(-self.dampy[None, 1:])
+                    )
+        self.ox = self.ox * np.exp(-self.dampx[:, None])
+        self.oy = self.oy * np.exp(-self.dampy[None:, ])
+        self.p = self.p * np.exp(- self.dampx[:-1, None])
         return None
-    
 
-    def PML(self,sigmamax=0.17):
-         
-         #thickness needs to be small relatively to nx
-         self.thickness=  max(1,self.nx//10)
+    def PML(self, sigmamax=0.17,thickness_denom = 10, pml_order = 1,prof_type = 'polynomial'):
 
+        # thickness needs to be small relatively to nx
+        self.thickness = max(1, self.nx // thickness_denom)
 
-        # damping coëfficient grid in x and t   
-         
-         
-         self.dampx=np.zeros(self.nx + 1) 
-         self.dampy=np.zeros(self.ny + 1)
+        # damping coëfficient grid in x and y
 
+        self.dampx = np.zeros(self.nx + 1)
+        self.dampy = np.zeros(self.ny + 1)
 
-         def profile(index,thickness,sigmamax): 
-              return sigmamax * ( (index /thickness) )
+        def profile(index, thickness, sigmamax,pml_order,prof_type):
+            return sigmamax * ((index / thickness) ** pml_order) if prof_type == 'polynomial' else (
+                    sigmamax * (1 - np.exp(-pml_order * (index / thickness)**4)))
 
-
-        
         # only uses the values in the pml layer so that the non pml layer doesnt get affected
-         for i in range(self.thickness):
-            sigma_value= profile(i,self.thickness,sigmamax)
-
+        for i in range(self.thickness):
+            sigma_value = profile(i, self.thickness, sigmamax,pml_order,prof_type)
 
             self.dampx[i] = sigma_value
             self.dampx[-(i + 1)] = sigma_value
             self.dampy[i] = sigma_value
             self.dampy[-(i + 1)] = sigma_value
-         return None
-
-
-
+        return None
 
     def iterate(self):
-        receiver1 = np.zeros((self.nt,1))
-        receiver2 = np.zeros((self.nt,1))
-        receiver3 = np.zeros((self.nt,1))
+        receiver1 = np.zeros((self.nt, 1))
+        receiver2 = np.zeros((self.nt, 1))
+        receiver3 = np.zeros((self.nt, 1))
 
-        timeseries = np.zeros((self.nt,1))
+        timeseries = np.zeros((self.nt, 1))
 
         fig, ax = plt.subplots()
         plt.axis('equal')
-        plt.xlim([1, self.nx+1])
-        plt.ylim([1, self.ny+1])
+        plt.xlim([1, self.nx + 1])
+        plt.ylim([1, self.ny + 1])
         movie = []
 
         for it in range(0, self.nt):
-            t = (it-1)*self.dt
+            t = (it - 1) * self.dt
             timeseries[it, 0] = t
             print('%d/%d' % (it, self.nt))  # Loading bar while sim is running
 
-            source = self.A * np.sin(2 * np.pi * self.fc * (t - self.t0)) * np.exp(-((t - self.t0) ** 2) / (self.sigma))  # Update source for new time
+            source = self.A * np.sin(2 * np.pi * self.fc * (t - self.t0)) * np.exp(
+                -((t - self.t0) ** 2) / (self.sigma))  # Update source for new time
 
             self.p[self.x_source, self.y_source] += source  # Adding source term to propagation
             self.step_SIT_SIP()  # Propagate over one time step
@@ -153,7 +137,7 @@ class FDTD():
                 ax.text(0.5, 1.05, '%d/%d' % (it, self.nt),
                         size=plt.rcParams["axes.titlesize"],
                         ha="center", transform=ax.transAxes),
-                ax.imshow(self.p, vmin=-0.02*self.A, vmax=0.02*self.A),  # Original animation code
+                ax.imshow(self.p, vmin=-0.02 * self.A, vmax=0.02 * self.A),  # Original animation code
                 ax.plot(self.x_source, self.y_source, 'rs', fillstyle="none")[0],
                 ax.plot(self.x_rec1, self.y_rec1, 'ko', fillstyle="none")[0],
                 ax.plot(self.x_rec2, self.y_rec2, 'ko', fillstyle="none")[0],
@@ -162,13 +146,12 @@ class FDTD():
             movie.append(artists)
         print('iterations done')
         my_anim = ArtistAnimation(fig, movie, interval=10, repeat_delay=1000,
-                                blit=True)
+                                  blit=True)
         plt.show()
         return None
 
 
+problem = FDTD()
 
-problem=FDTD()
-
-problem.PML()
+problem.PML(sigmamax=0.15,thickness_denom=6,pml_order=12,prof_type='apolynomial')
 problem.iterate()
